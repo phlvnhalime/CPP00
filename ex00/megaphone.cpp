a0e0685b588cfa801abb1dccb6c88fa651774b86
Halime Pehlivan <halimephlvn@MSI>
2025-05-20 16:56:31 +0200

    Welcome CPP, Finished ex00.. others is loading

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+
+using namespace std; // Import everything!
+char ft_toupper(char c);
+
+int main(int ac, char *av[])
+{
+    if(ac == 1)
+    {
+        cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << endl;
+    }
+    else
+    {
+        for(int i = 1 ; i < ac ; i++)
+        {
+
+            string arg(av[i]);
+            size_t j = 0;
+            while(j < arg.length()) // It is write everything without space
+            {
+                cout << ft_toupper(arg[j]);
+                j++;
+            }
+            // If i want to add space in here i have to add space
+            /*
+                if(i < ac -1)
+                {
+                    cout << " "; 
+                } // after the each arguments
+
+                BUTT FOR evey space i can see
+
+            */
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+
+char ft_toupper(char c)
+{
+    if(c >= 'a' && c <= 'z')
+    {
+        return (c - 32);
+    }
+    return (c);
+}
